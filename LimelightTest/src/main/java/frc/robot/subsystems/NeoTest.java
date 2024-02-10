040523bca67638526c527bba7c2b18069b451ae8
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2024-02-10 15:13:58 -0500

    Limelight testing

---
+++ LimelightTest/src/main/java/frc/robot/subsystems/NeoTest.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+
+public class NeoTest extends SubsystemBase {
+
+    // CANSparkMax motor = new CANSparkMax();
+
+
+
+}
