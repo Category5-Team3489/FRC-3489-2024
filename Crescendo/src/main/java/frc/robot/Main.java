501c3fc295b7f9e2e7b15b348cfe14eba8e713d9
Connor <61102529+Metater@users.noreply.github.com>
2024-02-25 18:37:01 -0500

    Lookup table code and start testing

--- Crescendo/src/main/java/frc/robot/Main.java
+++ Crescendo/src/main/java/frc/robot/Main.java
@@ -4,22 +4,36 @@
 //Nicholas was here
 package frc.robot;
 
-import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.util.datalog.DoubleLogEntry;
+import edu.wpi.first.wpilibj.DataLogManager;
+import frc.cat5lib.DistanceLookupTable;
 
 /**
- * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
- * you are doing, do not modify this file except to change the parameter class to the startRobot
+ * Do NOT add any static variables to this class, or any initialization at all.
+ * Unless you know what
+ * you are doing, do not modify this file except to change the parameter class
+ * to the startRobot
  * call.
  */
 public final class Main {
-  Main() {}
+    Main() {
+    }
 
-  /**
-   * Main initialization function. Do not perform any initialization here.
-   *
-   * <p>If you change your main robot class, change the parameter type.
-   */
-  public static void main(String... args) {
-    RobotBase.startRobot(Robot::new);
-  }
+    /**
+     * Main initialization function. Do not perform any initialization here.
+     *
+     * <p>
+     * If you change your main robot class, change the parameter type.
+     */
+    public static void main(String... args) {
+        DistanceLookupTable lookupTable = new DistanceLookupTable();
+
+        DoubleLogEntry logEntry = new DoubleLogEntry(DataLogManager.getLog(), "/debug/lookup-table");
+        for (double distanceMeters = 0; distanceMeters < 10; distanceMeters += 0.01) {
+            double estimatedOutputValue = lookupTable.estimateOutputValue(distanceMeters);
+            logEntry.append(estimatedOutputValue, (long) (distanceMeters * 1000000.0));
+        }
+
+        // edu.wpi.first.wpilibj.RobotBase.startRobot(Robot::new);
+    }
 }
