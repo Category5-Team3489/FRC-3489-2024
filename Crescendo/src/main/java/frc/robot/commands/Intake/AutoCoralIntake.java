ed68de580f0f70fb8649c7646334720a6b8ec5d3
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2024-04-05 23:33:24 -0400

    Trying to get auto intake working in auto --- not tested

--- Crescendo/src/main/java/frc/robot/commands/Intake/AutoCoralIntake.java
+++ Crescendo/src/main/java/frc/robot/commands/Intake/AutoCoralIntake.java
@@ -1,5 +1,162 @@
 package frc.robot.commands.Intake;
 
-public class AutoCoralIntake {
-    
+import java.util.function.DoubleSupplier;
+
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+
+import edu.wpi.first.units.Time;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants;
+import frc.robot.enums.IndexState;
+import frc.robot.enums.IntakeState;
+import frc.robot.enums.ShooterAngleState;
+import frc.robot.subsystems.CoralLimelight;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Index;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.ShooterAngle;
+
+public class AutoCoralIntake extends Command {
+    private final Drivetrain drivetrain = Drivetrain.get();
+    private final CoralLimelight coralLimelight = CoralLimelight.get();
+    private final Intake intake = Intake.get();
+    private final ShooterAngle shooterAngle = ShooterAngle.get();
+    private final Index index = Index.get();
+
+    private final IntakeUntilDetection intakeCommand = new IntakeUntilDetection();
+    private final SwerveRequest.RobotCentric drive = new SwerveRequest.RobotCentric();
+
+    Command driveCommandForward = drivetrain.applyRequest(() -> drive
+            .withVelocityX(getDrivetrainVelocityX())
+            .withVelocityY(getDrivetrainVelocityY())
+            .withRotationalRate(getDrivetrainAngleRate()));
+
+    // TODO Test
+    Command intakeCommandm = intake.intakeCommand(IntakeState.centerIn, IntakeState.falconIn);
+    Command shooterAngleCommand = shooterAngle.updateCommand(ShooterAngleState.Max.getAngle());
+    Command indexCommand = index.indexCommand(IndexState.Intake);
+
+    private double drivetrainAngleRate = 0;
+    private double drivetrainVelocityX = 0;
+    private double drivetrainVelocityY = 0;
+
+    public boolean shouldAutoIntake = true;
+    private boolean isFinished = false;
+
+    private double speed = (16.5 / 3.281) * 0.3;
+
+    private Timer intakeTimer = new Timer();
+
+    private double rotationSpeed = 0.12 * Constants.Drivetrain.MaxRadiansPerSecond;
+
+    // TODO Test Ranges
+    private final double tXRange = 16; // 3 -- 5
+    private final double tYRange = 5;
+
+    private double currentDegrees;
+
+    public AutoCoralIntake() {
+        Commands.runOnce(() -> Commands.parallel(
+                drivetrain.applyFieldCentricFacingAngle(() -> 0, () -> 0, () -> 0)));
+    }
+
+    @Override
+    public void initialize() {
+        // TODO Test
+        intakeCommandm.schedule();
+        indexCommand.schedule();
+        shooterAngleCommand.schedule();
+    }
+
+    @Override
+    public void execute() {
+        double targetX = coralLimelight.getTargetX();
+        double targetY = coralLimelight.getTargetY();
+        double targetV = coralLimelight.getTargetVisible();
+
+        // TODO Test Acuracy
+        // currentDegrees = drivetrain.getCurrentHeading();
+        // System.out.println("Current degree: " + currentDegrees);
+
+        // Return if april tag is not visible
+        if (targetV == 0) {
+            System.out.println("Note is not visible");
+            return;
+        }
+
+        // If within ty range
+        // if (targetY <= tYRange) {
+        // if within tx range
+        if (Math.abs(targetX) < tXRange) {
+            System.out.println("----EQUAL------");
+            isFinished = true;
+            return;
+
+        } else if (targetX < 0) { // if less than target
+            drivetrainAngleRate = -rotationSpeed;
+            driveCommandForward.schedule();
+            System.out.println("target < 0");
+            // isFinished = false;
+        } else if (targetX > 0) { // if greater than target
+            drivetrainAngleRate = rotationSpeed;
+            driveCommandForward.schedule();
+            System.out.println("target > 0");
+            // isFinished = false;
+
+        }
+        // }
+    }
+
+    // This works but does not allow the robot to corect if it overshoots
+    @Override
+    public boolean isFinished() {
+        // return isFinished;
+        return true;
+    }
+
+    // This works but does not allow the robot to corect if it overshoots
+    @Override
+    public void end(boolean interrupted) {
+        drivetrainAngleRate = 0;
+        drivetrainVelocityX = 0.2;
+        driveCommandForward.schedule();
+        System.out.println("===========================End");
+        shouldAutoIntake = false;
+    }
+
+    private double getDrivetrainAngleRate() {
+        return drivetrainAngleRate;
+    }
+
+    private double getDrivetrainVelocityX() {
+        return drivetrainVelocityX;
+    }
+
+    private double getDrivetrainVelocityY() {
+        return drivetrainVelocityY;
+    }
+
+    private double getXVelocityCalculation(double angle) {
+        return Math.sin(angle) * speed;
+    }
+
+    private double getYVelocityCalculation(double angle) {
+        return Math.cos(angle) * speed;
+    }
 }
+
+// press button
+// check if note is visible
+// if with in ty degree range
+// rotate depending on tx value until tx == 0
+// start intake
+// drive forward
+// wait for laser value then shooter = home
+// if outside
+// print error
+// return
+
+// is the current heading output correct?
+//
--- Crescendo/src/main/java/frc/robot/commands/autos/SourceCenterLineManual.java
+++ Crescendo/src/main/java/frc/robot/commands/autos/SourceCenterLineManual.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Cat5Utils;
 import frc.robot.Constants;
+import frc.robot.commands.Intake.AutoCoralIntake;
 import frc.robot.commands.Intake.CoralIntake;
 import frc.robot.commands.autoShooting.AutoShoot;
 import frc.robot.commands.shooter.SetShooterSpeedAndAngle;
@@ -21,126 +22,138 @@ import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.ShooterAngle;
 
 public class SourceCenterLineManual {
-        private Index index = Index.get();
-        private ShooterAngle shooterAngle = ShooterAngle.get();
-        private Intake intake = Intake.get();
-        private Drivetrain drivetrain = Drivetrain.get();
+    private Index index = Index.get();
+    private ShooterAngle shooterAngle = ShooterAngle.get();
+    private Intake intake = Intake.get();
+    private Drivetrain drivetrain = Drivetrain.get();
 
-        private static final double MaxMetersPerSecond = Constants.Drivetrain.MaxMetersPerSecond;
-        private static final double MaxRadiansPerSecond = Constants.Drivetrain.MaxRadiansPerSecond;
+    private static final double MaxMetersPerSecond = Constants.Drivetrain.MaxMetersPerSecond;
+    private static final double MaxRadiansPerSecond = Constants.Drivetrain.MaxRadiansPerSecond;
 
-        Trigger laserTrigger = new Trigger(index.laserSensor::get);
+    Trigger laserTrigger = new Trigger(index.laserSensor::get);
 
-        final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-                        .withDeadband(MaxMetersPerSecond * 0.1)
-                        .withRotationalDeadband(MaxRadiansPerSecond * 0.1)
-                        .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+    final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+            .withDeadband(MaxMetersPerSecond * 0.1)
+            .withRotationalDeadband(MaxRadiansPerSecond * 0.1)
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
-        Command autoShoot = new AutoShoot();
+    Command autoShoot = new AutoShoot();
 
-        Command autoIntakeCommand = new CoralIntake();
+    // Command autoIntakeCommand = new CoralIntake();
 
-        // Ensure percentages are greater than the 0.1 percent deadband above
-        // Domain is [-1, 1]
+    // Ensure percentages are greater than the 0.1 percent deadband above
+    // Domain is [-1, 1]
 
-        // Rotate/drive side
-        double percentY = Cat5Utils.Red(-0.6);
-        double percentX = 0;
-        double percentOmega = Cat5Utils.Red(-0.2);
-        double driveTimeSeconds = 0.8;
+    // Rotate/drive side
+    double percentY = Cat5Utils.Red(-0.6);
+    double percentX = 0;
+    double percentOmega = Cat5Utils.Red(-0.2);
+    double driveTimeSeconds = 0.8;
 
-        // wall to note center 30.5 in amp side
-        // intake drive
-        double percentY2 = Cat5Utils.Red(-0.3); //Blue alliance hit stage (make this larger?)  //22 was good for stage but too left of ring
-        double percentX2 = 0.6;
-        double percentOmega2 = 0.0;
-        double driveTimeSeconds2 = 6;
+    // wall to note center 30.5 in amp side
+    // intake drive
+    double percentY2 = Cat5Utils.Red(-0.3); // Blue alliance hit stage (make this larger?) //22 was good for stage
+                                            // but too left of ring
+    double percentX2 = 0.6;
+    double percentOmega2 = 0.0;
+    double driveTimeSeconds2 = 6;
 
-        // drive back
-        double percentY3 = Cat5Utils.Red(0.3);
-        double percentX3 = -0.6;
-        double percentOmega3 = 0;
-        double driveTimeSeconds3 = 5;
+    // drive back
+    double percentY3 = Cat5Utils.Red(0.3);
+    double percentX3 = -0.6;
+    double percentOmega3 = 0;
+    double driveTimeSeconds3 = 5;
 
-        // drive forward intake
-        double percentY4 = 0;
-        double percentX4 = -0.3;
-        double percentOmega4 = 0;
-        double driveTimeSeconds4 = 1;
+    // drive forward intake
+    double percentY4 = 0;
+    double percentX4 = -0.3;
+    double percentOmega4 = 0;
+    double driveTimeSeconds4 = 1;
 
-        double speedMultiplier = 0.5; // [0, 1]
+    double speedMultiplier = 0.5; // [0, 1]
 
-        Command shooterIndex = index.indexCommand(IndexState.Intake);
+    Command shooterIndex = index.indexCommand(IndexState.Intake);
 
-        Command closeShootCommand = new SetShooterSpeedAndAngle(
-                        Constants.ShooterAngle.CloseShooterAngle,
-                        Constants.ShooterSpeed.CloseShooterSpeed).withTimeout(2);
+    AutoCoralIntake autoCoralIntake = new AutoCoralIntake();
 
-        Command driveCommandSideRotate = drivetrain.applyRequest(() -> drive
-                        .withVelocityX(percentX * MaxMetersPerSecond * speedMultiplier)
-                        .withVelocityY(-percentY * MaxMetersPerSecond * speedMultiplier)
-                        .withRotationalRate(-percentOmega * MaxRadiansPerSecond * speedMultiplier));
+    Command autoCoralIntakeCommand = autoCoralIntake.onlyWhile(() -> autoCoralIntake.shouldAutoIntake);
 
-        Command driveCommandDriveCenter = drivetrain.applyRequest(() -> drive
-                        .withVelocityX(percentX2 * MaxMetersPerSecond * speedMultiplier)
-                        .withVelocityY(-percentY2 * MaxMetersPerSecond * speedMultiplier)
-                        .withRotationalRate(-percentOmega2 * MaxRadiansPerSecond * speedMultiplier));
+    Command closeShootCommand = new SetShooterSpeedAndAngle(
+            Constants.ShooterAngle.CloseShooterAngle,
+            Constants.ShooterSpeed.CloseShooterSpeed).withTimeout(2);
 
-        Command driveCommandIntake = drivetrain.applyRequest(() -> drive
-                        .withVelocityX(percentX4 * MaxMetersPerSecond * speedMultiplier)
-                        .withVelocityY(-percentY4 * MaxMetersPerSecond * speedMultiplier)
-                        .withRotationalRate(-percentOmega4 * MaxRadiansPerSecond * speedMultiplier));
+    Command driveCommandSideRotate = drivetrain.applyRequest(() -> drive
+            .withVelocityX(percentX * MaxMetersPerSecond * speedMultiplier)
+            .withVelocityY(-percentY * MaxMetersPerSecond * speedMultiplier)
+            .withRotationalRate(-percentOmega * MaxRadiansPerSecond * speedMultiplier));
 
-        Command driveCommandBack = drivetrain.applyRequest(() -> drive
-                        .withVelocityX(percentX3 * MaxMetersPerSecond * speedMultiplier)
-                        .withVelocityY(-percentY3 * MaxMetersPerSecond * speedMultiplier)
-                        .withRotationalRate(-percentOmega3 * MaxRadiansPerSecond * speedMultiplier));
+    Command driveCommandDriveCenter = drivetrain.applyRequest(() -> drive
+            .withVelocityX(percentX2 * MaxMetersPerSecond * speedMultiplier)
+            .withVelocityY(-percentY2 * MaxMetersPerSecond * speedMultiplier)
+            .withRotationalRate(-percentOmega2 * MaxRadiansPerSecond * speedMultiplier));
 
-        // Command driveCommandForward = drivetrain.applyRequest(() -> drive
-        // .withVelocityX(percentX4 * MaxMetersPerSecond * speedMultiplier)
-        // .withVelocityY(-percentY4 * MaxMetersPerSecond * speedMultiplier)
-        // .withRotationalRate(-percentOmega4 * MaxRadiansPerSecond * speedMultiplier));
+    Command driveCommandIntake = drivetrain.applyRequest(() -> drive
+            .withVelocityX(percentX4 * MaxMetersPerSecond * speedMultiplier)
+            .withVelocityY(-percentY4 * MaxMetersPerSecond * speedMultiplier)
+            .withRotationalRate(-percentOmega4 * MaxRadiansPerSecond * speedMultiplier));
 
-        public Command sourceCenterLineManual() {
-                // manual shoot
-                return Commands.parallel(closeShootCommand, Commands.waitSeconds(3))
+    Command driveCommandBack = drivetrain.applyRequest(() -> drive
+            .withVelocityX(percentX3 * MaxMetersPerSecond * speedMultiplier)
+            .withVelocityY(-percentY3 * MaxMetersPerSecond * speedMultiplier)
+            .withRotationalRate(-percentOmega3 * MaxRadiansPerSecond * speedMultiplier));
 
-                                .andThen(Commands.parallel(shooterIndex), Commands.waitSeconds(2))
+    // Command driveCommandForward = drivetrain.applyRequest(() -> drive
+    // .withVelocityX(percentX4 * MaxMetersPerSecond * speedMultiplier)
+    // .withVelocityY(-percentY4 * MaxMetersPerSecond * speedMultiplier)
+    // .withRotationalRate(-percentOmega4 * MaxRadiansPerSecond * speedMultiplier));
 
-                                // rotate to forward and drive to the side away from the speaker
-                                .andThen(driveCommandSideRotate.withTimeout(driveTimeSeconds))
+    public Command sourceCenterLineManual() {
+        // manual shoot
+        return Commands.parallel(closeShootCommand, Commands.waitSeconds(3))
 
-                                // Start intake
-                                .andThen(() -> shooterAngle.updateCommand(ShooterAngleState.Max.getAngle()).schedule())
-                                .andThen(intake.intakeCommand(IntakeState.centerIn, IntakeState.falconIn))
-                                .andThen(index.indexCommand(IndexState.Intake))
+                .andThen(Commands.parallel(shooterIndex), Commands.waitSeconds(2))
 
-                                // Drive forward
-                                .andThen(driveCommandDriveCenter.withTimeout(driveTimeSeconds2))
-                                // Cancle the drive command
-                                .andThen(() -> driveCommandDriveCenter.cancel())
+                // rotate to forward and drive to the side away from the speaker
+                .andThen(driveCommandSideRotate.withTimeout(driveTimeSeconds))
 
-                                .andThen(Commands.print("Drive Done"))
+                // Start intake
+                .andThen(() -> shooterAngle.updateCommand(ShooterAngleState.Max.getAngle()).schedule())
+                .andThen(intake.intakeCommand(IntakeState.centerIn, IntakeState.falconIn))
+                .andThen(index.indexCommand(IndexState.Intake))
 
-                                // Drive forward to intake
-                                .andThen(driveCommandIntake.withTimeout(driveTimeSeconds4))
+                // Drive forward
+                .andThen(driveCommandDriveCenter.withTimeout(driveTimeSeconds2))
+                // Cancle the drive command
+                .andThen(() -> driveCommandDriveCenter.cancel())
 
-                                // Stop intake when piece detected
-                                .andThen(() -> laserTrigger
-                                                // .debounce(0.29, DebounceType.kRising)
-                                                .onTrue(Commands.runOnce(() -> {
-                                                        if (intake.hasIntakeBeenSet) {
-                                                                intake.stop();
-                                                                index.stop();
-                                                        }
-                                                })))
+                .andThen(Commands.print("Drive Done"))
 
-                                // drive back to see apriltag
-                                .andThen(driveCommandBack.withTimeout(driveTimeSeconds3))
+                // Drive forward to intake
+                // .andThen(driveCommandIntake.withTimeout(driveTimeSeconds4))
 
-                                // Auto shoot
-                                .andThen(() -> autoShoot.schedule())
+                // .andThen(() -> autoCoralIntakeCommand.schedule())
 
-                                .withName("SourceCenterLineManual");
-        }
+                // TODO Test if the other one does not work
+                .andThen(autoCoralIntakeCommand)
+
+                .andThen(Commands.print("Auto Intake Done"))
+
+                //TODO possibly update time to stop auto intake
+                // Stop intake when piece detected
+                .andThen(() -> laserTrigger
+                        .onTrue(Commands.runOnce(() -> {
+                            if (intake.hasIntakeBeenSet) {
+                                index.stop();
+                                intake.stop();
+                            }
+                        })))
+
+                // drive back to see apriltag
+                .andThen(driveCommandBack.withTimeout(driveTimeSeconds3))
+
+                // Auto shoot
+                .andThen(() -> autoShoot.schedule())
+
+                .withName("SourceCenterLineManual");
+    }
 }
